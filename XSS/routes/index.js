7ef9a44a7ea467741616edeb4f5b236bbd7d7bca
lxk <2664276518@qq.com>
2018-02-27 23:17:43 +0800

    add xss

--- XSS/routes/index.js
+++ XSS/routes/index.js
@@ -3,30 +3,33 @@ var router = express.Router();
 
 var comments = {}
 
+// 编码
 function html_encode(str) {
-  if (str.length == 0) return '';
-  str = str.replace(/</g, '&lt;');
-  str = str.replace(/>/g, '&gt;');
-  str = str.replace(/\'/g, '&39;');
-  str = str.replace(/\"/g, '&quot;');
-  str = str.replace(/\s/g, '&nbsp;');
-  str = str.replace(/\n/g, '<br>');
-  return str
+    if (str.length == 0) return '';
+    var s = ''
+    s = str.replace(/</g, '&lt;');
+    s = s.replace(/>/g, '&gt;');
+    s = s.replace(/\'/g, '&#39;');
+    s = s.replace(/\"/g, '&quot;');
+    s = s.replace(/\s/g, '&nbsp;');
+    s = s.replace(/\n/g, '<br>');
+    return s
 }
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {title: 'Express', xss: req.query.xss});
+    res.render('index', {title: 'Express', xss: req.query.xss});
 });
 
 router.get('/setComment', function (req, res, next) {
-  comments.v = html_encode(req.query.comment)
+    comments.v = html_encode(req.query.comment)
+    res.end()
 });
 
 router.get('/getComment', function (req, res, next) {
-  res.json({
-    comment: comments.v
-  })
+    res.json({
+        comment: comments.v
+    })
 });
 
 module.exports = router;

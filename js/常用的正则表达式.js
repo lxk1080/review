fa0004f479e4095cdd54fdc3a19dfa51f810e695
lxkee <lixk@shukun.net>
2019-03-10 00:11:22 +0800

    add resource_regex_CN

---
+++ js/常用的正则表达式.js
@@ -0,0 +1,5 @@
+// 匹配中文字符（只能在google支持，其他的用插件，详见./resource/JavaScript正则表达式匹配汉字.js）
+const regex = /\p{Unified_Ideograph}/u;
+console.log(regex.test('中文')); // true
+console.log(regex.test('123中文qwe')); // true
+console.log(regex.test('123qwe')); // false

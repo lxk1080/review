7d5c48eb197a6b74cf37d0ee5bb75577ca9fa92b
unknown <2664276518@qq.com>
2021-09-29 20:41:57 +0800

    update jsIQ

--- jsIQ/排列组合.js
+++ jsIQ/排列组合.js
@@ -1,11 +1,38 @@
 
-function getCombination(arr) {
-  let resultArr = arr[0].slice();
-  arr.forEach((item) => {
-    if (!resultArr.length) {
-      resultArr = arr;
+/**
+ * 给出一个数组，长度不定，内部的子数组也长度不定，每次从各个子数组中取出一个数据，列出所有的组合
+ * @param array
+ * @returns {Array}
+ */
+function getCombination(array) {
+  let resultArr = []
+  array.forEach((arrItem) => {
+    if (resultArr.length === 0) {
+      let firstItem = []
+      arrItem.forEach(item => {
+        firstItem.push(item)
+      })
+      resultArr = firstItem
     } else {
-
+      const emptyArray = []
+      resultArr.forEach((item) => {
+        arrItem.forEach((value) => {
+          emptyArray.push(item + value)
+        })
+      })
+      resultArr = emptyArray
     }
   })
+  return resultArr
 }
+
+
+
+// 以下是测试 -----------------------------------------------------------------------------------------------------------
+
+const list = [[1, 2, 3], ['a', 'b', 'c'], ['d', 'e']]
+
+const res = getCombination(list)
+
+console.log('res:', res)
+console.log('length:', res.length) // 3 * 3 * 2 = 18
--- test.js
+++ test.js
@@ -1,24 +1,2 @@
 
-let list = [[1, 2, 3], ['a', 'b', 'c'], ['d', 'e']]
-function getCombination(array) {
-  let resultArr = []
-  array.forEach((arrItem) => {
-    if (resultArr.length === 0) {
-      let firstItem = []
-      arrItem.forEach(item => {
-        firstItem.push(item)
-      })
-      resultArr = firstItem
-    } else {
-      const emptyArray = []
-      resultArr.forEach((item) => {
-        arrItem.forEach((value) => {
-          emptyArray.push(item + value)
-        })
-      })
-      resultArr = emptyArray
-    }
-  })
-  return resultArr
-}
-console.log(getCombination(list))
+

50011173d10d3dc5e5f55bf26354678a01996be5
zerol <2664276518@qq.com>
2024-03-11 20:00:36 +0800

    chore: update react about

--- React/react-z/src/components/使用context生产数据/ThemedButton.js
+++ React/react-z/src/components/使用context生产数据/ThemedButton.js
@@ -4,10 +4,13 @@ import ThemeContext from "./config";
 // 底层组件 - class 组件
 class ThemedButton extends React.Component {
   // 指定 contextType 读取当前的 theme context
-  static contextType = ThemeContext // 也可以在 class 外写 ThemedButton.contextType = ThemeContext
+  // 也可以在 class 外写 ThemedButton.contextType = ThemeContext
+  // 注意这一句是一定要写的，不止是定义个类型那么简单，如果不写，就拿不到值
+  static contextType = ThemeContext
 
   render() {
-    const data = this.context // React 会往上找到 theme Provider，然后使用它的值
+    // React 会往上找到 theme Provider，然后使用它的值
+    const data = this.context
 
     return <div>
       <p>button's theme is {data.theme}</p>

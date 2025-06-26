856f750daf430f060278bab7406cd85204f41073
zerol <2664276518@qq.com>
2025-06-26 18:30:22 +0800

    chore: update review-interview

--- nodejs/开启多进程/使用cluster实现/index.js
+++ nodejs/开启多进程/使用cluster实现/index.js
@@ -12,7 +12,7 @@ if (cluster.isMaster) {
     cluster.fork() // 进程守护（少了一个，就再开一个）
   })
 } else {
-  // 多个子进程会共享一个 TCP 连接，提供一份网络服务
+  // 多个子进程会共享一个 TCP 连接，提供一份网络服务（也就是说对外来看就一个）
   const server = http.createServer((req, res) => {
     res.writeHead(200)
     res.end('Done')
